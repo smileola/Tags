a1cc62d560e97f841ce63e7db78e1449f22a7a21
smileola <alaoola@gmail.com>
2016-10-20 16:08:27 -0400

    Condition is now a abstract method Created the AcceuilTags Activity

---
+++ app/src/main/java/toto/tags/AcceuilTags.java
@@ -0,0 +1,13 @@
+package toto.tags;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcceuilTags extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil_tags);
+    }
+}
--- app/src/main/java/toto/tags/Condition.java
+++ app/src/main/java/toto/tags/Condition.java
@@ -3,9 +3,13 @@ package toto.tags;
 /**
  * Created by Toto on 2016-09-19.
  */
-public interface Condition {
+public abstract class Condition {
     //en faire une classe abstraite
-    //private boolean state;
-	public boolean setState();
-    public boolean getState();
+    private boolean state = false;
+	public void setState(boolean state){
+        this.state = state;
+    }
+    public boolean getState(){
+        return state;
+    }
 }
